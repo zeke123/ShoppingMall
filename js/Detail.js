eafee4918cd3786398f7824d8e1aad3e04839e5a
周建(88405798) <88405798@suning.com>
2019-03-11 17:00:00 +0800

    Revert "Revert "添加商品详情页""
    This reverts commit 3d38c6d5e6920e354d13565258c16528589924e4.

--- js/Detail.js
+++ js/Detail.js
@@ -4,6 +4,7 @@ import {
     StyleSheet,
     Text,
     View,
+    TouchableOpacity,
 } from 'react-native';
 
 
@@ -14,26 +15,42 @@ export default class Detail extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>
-                    详情界面
-                </Text>
+
+                <TouchableOpacity onPress={() => this.backUp()}>
+                    
+                    <Text style={styles.text}>
+                        详情界面
+                    </Text>
+                </TouchableOpacity>
+
             </View>
         );
     }
+
+    // 返回上一页面
+    backUp(){
+        //从props取出navigator
+        const {navigator} = this.props;
+        if (navigator) {
+            navigator.pop();
+        }
+        
+    }
+    
 }
 
 const styles = StyleSheet.create({
 
-    container:{
-        flex:1,
-        backgroundColor:'gray',
-        justifyContent:'center',
-        alignItems:'center',
+    container: {
+        flex: 1,
+        backgroundColor: 'gray',
+        justifyContent: 'center',
+        alignItems: 'center',
 
     },
 
-    text:{
-        fontSize:20,
+    text: {
+        fontSize: 20,
     }
 
 });
